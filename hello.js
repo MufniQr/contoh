35a883c7ea8188f9c95767ac40159ad79ca6278b
MufniQr <mufnianbiya01@gmail.com>
2023-01-02 19:01:09 +0700

    baru

---
+++ hello.js
@@ -0,0 +1,6 @@
+var obj = ["ayam", "gajah", "ayam", "jerapah", "jerapah", "cacing"];
+
+var unik = Array.from(new Set(obj));
+
+console.log(obj + " (belum diperbaiki) \n");
+console.log(unik + " (sudah diperbauiki)");
